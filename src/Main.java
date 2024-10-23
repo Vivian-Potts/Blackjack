8a3a32b9abbfda4ddc0ded2aaa8cd534e1feb501
Vivian.Potts <luc.potts@outlook.com>
2024-10-23 15:47:57 +0100

    1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //Make a deck object
+        CardDeck deck = new CardDeck();
+        //Print every card in deck
+        for (int www = 0; www<51; www++) {
+            System.out.println(deck.deal());
+        }
+    }
+
+    public static void play() {
+        //TO BE CONTINUED
+
+    }
+}
